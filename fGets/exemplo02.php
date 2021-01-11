58bc7a05ef70d543dc1711f0a4fcd116f7ad5deb
samantaSouza357 <samantasouza931@gmail.com>
2021-01-11 08:30:11 -0300

    curso

--- fGets/exemplo02.php
+++ fGets/exemplo02.php
@@ -1,10 +1,18 @@
 <?php
 
+$filename = "logo.png";
 
+$base64 = base64_encode(file_get_contents($filename));
 
+$fileInfo = new finfo(FILEINFO_MIME_TYPE);
 
+$mimetype = $fileInfo->file($filename);
 
-
+$base64encode =  "data:" . $mimetype . ";base64," . $base64;
 
 
 ?>
+
+<img src="<?= $base64encode ?>">
+
+<a href="<?= $base64encode ?>" target="_blank">Link para imagem</a>
